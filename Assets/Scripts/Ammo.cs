91be3ebd0dc3aae2e86def2271fe663bf062a17e
Majid Moghadam <majid.moghadam114@yahoo.com>
2021-10-27 10:53:40 -0400

    Chapter 11 - Added UI Coding

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -71,5 +71,7 @@ public class Ammo : MonoBehaviour
             Debug.LogError("Unrecognized gun type passed:" + tag);
         }
         tagToAmmo[tag]--;
+
+        gameUI.SetAmmoText(tagToAmmo[tag]);
     }
 }
--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -81,6 +81,7 @@ public class Robot : MonoBehaviour
             isDead = true;
             robot.Play("Die");
             StartCoroutine("DestroyRobot");
+            Game.RemoveEnemy();
             GetComponent<AudioSource>().PlayOneShot(deathSound);
         }
         else
